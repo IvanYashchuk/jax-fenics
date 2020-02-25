effd354a991505354ea9d602b9d1be8d3787c804
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-02-25 07:48:14 +0200

    Support only Python 3

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from distutils.core import setup
 
+import sys
+
+if sys.version_info[0] < 3:
+    raise Exception(
+        "The jaxfenics only supports Python3. Did you run $python setup.py <option>.? Try running $python3 setup.py <option>."
+    )
+
 setup(
     name="jaxfenics",
     description="JAX-FEniCS interface",
