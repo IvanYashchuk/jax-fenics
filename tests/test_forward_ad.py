ea37b93b15edeb62a272d2b98a9e123dbe0f97ad
Ivan Yashchuk <ivan.yashchuk@aalto.fi>
2020-03-05 10:22:37 +0200

    Improved tests for solve AD

--- tests/test_forward_ad.py
+++ tests/test_forward_ad.py
@@ -1,4 +1,5 @@
 from pytest_check import check
+import pytest
 import fdm
 import jax
 from jax.config import config
@@ -17,7 +18,7 @@ mesh = fenics.UnitSquareMesh(3, 2)
 V = fenics.FunctionSpace(mesh, "P", 1)
 
 
-def solve_fenics(kappa0, kappa1):
+def solve_fenics(q, kappa0, kappa1):
 
     f = fenics.Expression(
         "10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", degree=2
@@ -27,40 +28,43 @@ def solve_fenics(kappa0, kappa1):
     bcs = [fenics.DirichletBC(V, fenics.Constant(0.0), "on_boundary")]
 
     inner, grad, dx = ufl.inner, ufl.grad, ufl.dx
-    JJ = 0.5 * inner(kappa0 * grad(u), grad(u)) * dx - kappa1 * f * u * dx
+    JJ = 0.5 * inner(kappa0 * grad(u), grad(u)) * dx - q * kappa1 * f * u * dx
     v = fenics.TestFunction(V)
     F = fenics.derivative(JJ, u, v)
     fenics.solve(F == 0, u, bcs=bcs)
     return u, F, bcs
 
 
-templates = (fenics.Constant(0.0), fenics.Constant(0.0))
-jax_fem_eval = build_jax_solve_eval_fwd(templates)(solve_fenics)
+templates = (fenics.Function(V), fenics.Constant(0.0), fenics.Constant(0.0))
+inputs = (np.ones(V.dim()), np.ones(1), np.ones(1) * 1.2)
+jax_solve_eval = build_jax_solve_eval_fwd(templates)(solve_fenics)
 
 # multivariate output function
-ff = lambda x, y: np.sqrt(np.square(jax_fem_eval(np.sqrt(x ** 3), y)))  # noqa: E731
-x_input = np.ones(1)
-y_input = 1.2 * np.ones(1)
-
-# multivariate output function of the first argument
-hh = lambda x: ff(x, y_input)  # noqa: E731
-# multivariate output function of the second argument
-gg = lambda y: ff(x_input, y)  # noqa: E731
+ff = lambda x, y, z: np.sqrt(  # noqa: E731
+    np.square(jax_solve_eval(x, np.sqrt(y ** 3), z))
+)
+ff0 = lambda x: ff(x, inputs[1], inputs[2])  # noqa: E731
+ff1 = lambda y: ff(inputs[0], y, inputs[2])  # noqa: E731
+ff2 = lambda z: ff(inputs[0], inputs[1], z)  # noqa: E731
 
 
 def test_jvp():
-    dir_v = 0.432543 * np.ones_like(x_input)
-    fdm_jvp = fdm.jvp(hh, dir_v)(x_input)
-    jax_jvp = jax.jvp(hh, (x_input,), (dir_v,))[1]
+    for func, inp in zip((ff0, ff1, ff2), inputs):
+        dir_v = 0.432543 * np.ones_like(inp)
+        fdm_jvp = fdm.jvp(func, dir_v)(inp)
+        jax_jvp = jax.jvp(func, (inp,), (dir_v,))[1]
+        with check:
+            assert np.allclose(fdm_jvp, jax_jvp)
 
-    assert np.allclose(fdm_jvp, jax_jvp)
 
-
-# for input of size one we can get full jacobian using jvp
+# TODO: jax.jacfwd outputs zero
+# JAX Tracer goes inside fenics wrappers and zero array is returned
+# because fenics numpy conversion works only for concrete arrays
+@pytest.mark.xfail
 def test_jacobian():
-    jax_fwd_jac = jax.jvp(
-        ff, (x_input, y_input), (np.ones_like(x_input), np.ones_like(y_input))
-    )[1]
-    fdm_jac0 = fdm.jacobian(hh)(x_input)
-    fdm_jac1 = fdm.jacobian(gg)(y_input)
-    assert np.allclose(fdm_jac0 + fdm_jac1, jax_fwd_jac[:, None])
+    # skipping ff0 as it is expensive with fdm
+    for func, inp in zip((ff1, ff2), (inputs[1], inputs[2])):
+        jax_jac = jax.jacfwd(func)(inp)
+        fdm_jac = fdm.jacobian(func)(inp)
+        with check:
+            assert np.allclose(jax_jac, fdm_jac)
--- tests/test_reverse_ad.py
+++ tests/test_reverse_ad.py
@@ -17,7 +17,7 @@ mesh = fenics.UnitSquareMesh(3, 2)
 V = fenics.FunctionSpace(mesh, "P", 1)
 
 
-def solve_fenics(kappa0, kappa1):
+def solve_fenics(q, kappa0, kappa1):
 
     f = fenics.Expression(
         "10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", degree=2
@@ -27,78 +27,65 @@ def solve_fenics(kappa0, kappa1):
     bcs = [fenics.DirichletBC(V, fenics.Constant(0.0), "on_boundary")]
 
     inner, grad, dx = ufl.inner, ufl.grad, ufl.dx
-    JJ = 0.5 * inner(kappa0 * grad(u), grad(u)) * dx - kappa1 * f * u * dx
+    JJ = 0.5 * inner(kappa0 * grad(u), grad(u)) * dx - q * kappa1 * f * u * dx
     v = fenics.TestFunction(V)
     F = fenics.derivative(JJ, u, v)
     fenics.solve(F == 0, u, bcs=bcs)
     return u, F, bcs
 
 
-templates = (fenics.Constant(0.0), fenics.Constant(0.0))
+templates = (fenics.Function(V), fenics.Constant(0.0), fenics.Constant(0.0))
+inputs = (np.ones(V.dim()), np.ones(1), np.ones(1) * 1.2)
 jax_solve_eval = build_jax_solve_eval(templates)(solve_fenics)
 
 # multivariate output function
-ff = lambda x, y: np.sqrt(np.square(jax_solve_eval(np.sqrt(x ** 3), y)))  # noqa: E731
-x_input = np.ones(1)
-y_input = 1.2 * np.ones(1)
-
-# multivariate output function of the first argument
-hh = lambda x: ff(x, y_input)  # noqa: E731
-# multivariate output function of the second argument
-gg = lambda y: ff(x_input, y)  # noqa: E731
+ff = lambda x, y, z: np.sqrt(  # noqa: E731
+    np.square(jax_solve_eval(x, np.sqrt(y ** 3), z))
+)
+ff0 = lambda x: ff(x, inputs[1], inputs[2])  # noqa: E731
+ff1 = lambda y: ff(inputs[0], y, inputs[2])  # noqa: E731
+ff2 = lambda z: ff(inputs[0], inputs[1], z)  # noqa: E731
 
 
 def test_jacobian():
-    fdm_jac0 = fdm.jacobian(hh)(x_input)
-    jax_jac0 = jax.jacrev(hh)(x_input)
-
-    with check:
-        assert np.allclose(fdm_jac0, jax_jac0)
-
     rngkey = jax.random.PRNGKey(0)
     v = jax.random.normal(rngkey, shape=(V.dim(),), dtype="float64")
+    # skipping ff0 as it is expensive with fdm
+    for func, inp in zip((ff1, ff2), (inputs[1], inputs[2])):
+        fdm_jac = fdm.jacobian(func)(inp)
+        jax_jac = jax.jacrev(func)(inp)
 
-    fdm_vjp0 = v @ fdm_jac0
-    jax_vjp0 = jax.vjp(hh, x_input)[1](v)
+        with check:
+            assert np.allclose(fdm_jac, jax_jac)
 
-    with check:
-        assert np.allclose(fdm_vjp0, jax_vjp0)
+        fdm_vjp = v @ fdm_jac
+        jax_vjp = jax.vjp(func, inp)[1](v)
 
-    fdm_jac1 = fdm.jacobian(gg)(y_input)
-    jax_jac1 = jax.jacrev(gg)(y_input)
-
-    with check:
-        assert np.allclose(fdm_jac1, jax_jac1)
-
-    rngkey = jax.random.PRNGKey(1)
-    v = jax.random.normal(rngkey, shape=(V.dim(),), dtype="float64")
-
-    fdm_vjp1 = v @ fdm_jac1
-    jax_vjp1 = jax.vjp(gg, y_input)[1](v)
-
-    with check:
-        assert np.allclose(fdm_vjp1, jax_vjp1)
+        with check:
+            assert np.allclose(fdm_vjp, jax_vjp)
 
 
 # scalar output function
-f_scalar = lambda x, y: np.sqrt(  # noqa: E731
-    np.sum(np.square(jax_solve_eval(np.sqrt(x ** 3), y)))
+f_scalar = lambda x, y, z: np.sqrt(  # noqa: E731
+    np.sum(np.square(jax_solve_eval(x, np.sqrt(y ** 3), z)))
 )
-h_scalar = lambda x: f_scalar(x, y_input)  # noqa: E731
+h_scalar = lambda y: f_scalar(inputs[0], y, inputs[2])  # noqa: E731
+fs0 = lambda x: f_scalar(x, inputs[1], inputs[2])  # noqa: E731
+fs1 = lambda y: f_scalar(inputs[0], y, inputs[2])  # noqa: E731
+fs2 = lambda z: f_scalar(inputs[0], inputs[1], z)  # noqa: E731
 
 
 def test_grad():
-    fdm_grad = fdm.gradient(h_scalar)(x_input)
-    jax_grad = jax.grad(h_scalar)(x_input)
+    fdm_grads = []
+    for func, inp in zip((fs0, fs1, fs2), inputs):
+        fdm_grad = fdm.gradient(func)(inp)
+        fdm_grads.append(fdm_grad)
+        jax_grad = jax.grad(func)(inp)
 
-    with check:
-        assert np.allclose(fdm_grad, jax_grad)
+        with check:
+            assert np.allclose(fdm_grad, jax_grad)
 
-    jax_grads = jax.grad(f_scalar, (0, 1))(x_input, y_input)
-    fdm_grad0 = fdm_grad
-    fdm_grad1 = fdm.gradient(lambda y: f_scalar(x_input, y))(y_input)  # noqa: E731
-
-    with check:
-        assert np.allclose(fdm_grad0, jax_grads[0])
-    with check:
-        assert np.allclose(fdm_grad1, jax_grads[1])
+    jax_grads = jax.grad(f_scalar, (0, 1, 2))(*inputs)
+    for i, fdm_grad in enumerate(fdm_grads):
+        with check:
+            assert np.allclose(fdm_grad, jax_grads[i])
